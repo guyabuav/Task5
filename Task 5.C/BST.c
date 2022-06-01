d24aff84c273b7e3bf6edd46a3a70f952bcf52e7
guyabuav <100515287+guyabuav@users.noreply.github.com>
2022-06-01 13:47:45 +0300

    commit

--- Task 5.C/BST.c
+++ Task 5.C/BST.c
@@ -14,23 +14,23 @@ do { \
 
 void initBST(BST* bst) {
 	
-	bst->root = NULL;
+	bst->root = NULL; // Init new BST
 	
 	}
 
 
 void insertBST(BST* bst, int value) {
-	if (bst->root == NULL) { // the tree is empty
-		TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
-		AllocationCheck(temp);
-		temp->element = value;
+	if (bst->root == NULL) { // if the tree is empty
+		TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode)); // memory allocation for new node
+		AllocationCheck(temp); // memory allocation by MACRO
+		temp->element = value; // insert new value to the new node
 		bst->root = temp;
 		bst->root->left = NULL;
 		bst->root->right = NULL;
 		return;
 	}
 	else {
-		if(value > bst->root->element)
+		if(value > bst->root->element) // arrange elements by order if binray tree
 		return insertBST(&bst->root->right, value);
 		if (value <= bst->root->element)
 			return insertBST(&bst->root->left, value);
@@ -39,17 +39,17 @@ void insertBST(BST* bst, int value) {
 
 void printTreeInorder(BST* bst) {
 	if (bst->root != NULL) {
-		printTreeInorder(&bst->root->left);
+		printTreeInorder(&bst->root->left); // get to the left nodes
 		printf("%d", bst->root->element);
-		printTreeInorder(&bst->root->right);
+		printTreeInorder(&bst->root->right); // get to the right nodes
 	}
 	
 }
 
 void destroyBST(BST* bst) {
 	if (bst->root != NULL) {
-		destroyBST(&bst->root->left);
-		destroyBST(&bst->root->right);
+		destroyBST(&bst->root->left); // get to the left nodes
+		destroyBST(&bst->root->right); // get to the right nodes
 		free(bst->root);
 	}
 
@@ -57,7 +57,7 @@ void destroyBST(BST* bst) {
 
 int findIndexNFromLast(BST* bst, int N) {
 	int all = CountRoots(bst);
-	if (N > all || N < 1) {
+	if (N > all || N < 1) { // if N is not in range
 		printf("parameter N is not in range");
 		return 0;
 	}
@@ -71,21 +71,21 @@ int findIndexNFromLast(BST* bst, int N) {
 	
 
 int mid(TreeNode* node) {
-	int all = CountRoots2(node);
-	int right = CountRoots2(node->right);
-	int middle = all - right;
+	int all = CountRoots2(node); // count all roots
+	int right = CountRoots2(node->right); // count only roots of node.right
+	int middle = all - right; // middle = roots of node.left
 	return middle;
 }
 int foo(BST* bst, int middle, int N) {
 	if (bst->root != NULL) {
 		int num = 0;
-		if (N == middle) {
+		if (N == middle) { // if N = roots of node.left, return the current element.
 			return bst->root->element;
 		}
-		if (middle < N) {
+		if (middle < N) { // if N > middle, we need nodes from right part of the tree
 			num = foo(&bst->root->right, middle + mid(bst->root->right) , N);
 		}
-		if (middle > N) {
+		if (middle > N) { // in N < middle, we need nodes from left part of the tree
 			num = foo(&bst->root->left, middle - mid(bst->root->left), N);
 		}
 		return num;
@@ -95,20 +95,36 @@ int foo(BST* bst, int middle, int N) {
 	}
 }
 
+bool CheckLeafLevel(BST* bst, int level, int* leaflevel) {
+	if (bst->root == NULL) { // if the tree is empty, every path is equal
+		return true;
+	}
+	if (bst->root->left == NULL && bst->root->right == NULL) { // if it is a leaf
+		if (*leaflevel == 0) { // if its the first path we counting
+			*leaflevel = level; // set first leaf level (we dont need this iteration because we have allready LEFT-PATH)
+			return true;
+		}
+		return (level == *leaflevel); // if its not the first leaf, compare it to the last leaflevel we found.
+	}
+	return CheckLeafLevel(&bst->root->left, level + 1, leaflevel) && CheckLeafLevel(&bst->root->right, level + 1, leaflevel);
+	// if this is not a leaf, check his left && right
+	}
+
+
 int sameHeightLeaves(BST* bst) {
-	
-	
-	
+	int* leaflevel = (int*)malloc(sizeof(int));
+	*(leaflevel) = countLeftPath(&bst->root); // count the nodes in left path in bst
+	return (CheckLeafLevel(&bst->root, 0, leaflevel));
 }
 
-int CountRoots(BST* bst) {
+int CountRoots(BST* bst) { // count roots in bst by bst
 	if (bst->root == NULL) {
 		return 0;
 	}
 	return 1 + CountRoots(&bst->root->left) + CountRoots(&bst->root->right);
 }
 
-int CountRoots2(TreeNode* node) {
+int CountRoots2(TreeNode* node) { // count roots in bst by treenode
 	if (node == NULL ){
 		return 0;
 	}
@@ -116,23 +132,10 @@ int CountRoots2(TreeNode* node) {
 
 }
 
-int countLeftPath(BST* bst) {
+int countLeftPath(BST* bst) { // count the left path of your bst.
 	if (bst == NULL) {
 		return 0;
 	}
 	return countLeftPath(bst->root->left) + 1;
 }
 
-bool CheckLeafLevel(BST* bst, int level, int* leaflevel) {
-	if (bst->root == NULL) {
-		return true;
-	}
-	if (bst->root->left == NULL && bst->root->right == NULL) {
-		if (*leaflevel == 0) {
-			*leaflevel = level;
-			return true;
-		}
-		return (level == *leaflevel);
-	}
-	return CheckLeafLevel(&bst->root->left, level + 1, leaflevel) && CheckLeafLevel(&bst->root->right, level + 1, leaflevel);
-	}
--- Task 5.C/Main.c
+++ Task 5.C/Main.c
@@ -8,7 +8,6 @@
 
 void main() {
 	BST bst;
-	int* leaflevel = (int*)malloc(sizeof(int));
 	initBST(&bst);
 	insertBST(&bst, 3);
 	insertBST(&bst, 1);
@@ -17,15 +16,13 @@ void main() {
 	insertBST(&bst, 5);
 	insertBST(&bst, 4);
 	insertBST(&bst, 8);
-	insertBST(&bst, 9);
+	//insertBST(&bst, 9);
 	print_ascii_tree(bst.root);
 	printTreeInorder(&bst);
-	
 	printf("\n");
 	printf("The number of roots is %d\n" , CountRoots(&bst.root));
 	printf("The NfromLAST number is %d\n" , findIndexNFromLast(&bst, 5));
-	*(leaflevel) = countLeftPath(&bst.root);
-	if (CheckLeafLevel(&bst.root, 0, leaflevel)) {
+	if (sameHeightLeaves(&bst.root)) {
 		printf("SameLevel for every path\n");
 	}
 	else printf("Not the same level for every path\n");
