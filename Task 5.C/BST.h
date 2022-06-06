57ba30b94800259dd1146b7bfe2d723be46cff85
guyabuav <100515287+guyabuav@users.noreply.github.com>
2022-06-06 21:33:42 +0300

    removed files

--- Task 5.C/BST.h
+++ Task 5.C/BST.h
@@ -1,6 +1,6 @@
 #ifndef _BST_ADT_H
 #define _BST_ADT_H
-
+#include <stdbool.h>
 typedef struct TreeNode TreeNode;
 
 struct TreeNode
@@ -16,3 +16,16 @@ typedef struct BST
 }BST;
 
 #endif // !_BST_ADT_H
+
+void initBST(BST* bst);
+void insertBST(BST* bst, int value);
+void printTreeInorder(BST* bst);
+void destroyBST(BST* bst);
+int findIndexNFromLast(BST* bst, int N);
+int mid(TreeNode* node);
+int foo(BST* bst, int middle, int N);
+bool CheckLeafLevel(BST* bst, int level, int* leaflevel);
+int sameHeightLeaves(BST* bst);
+int CountRoots(BST* bst);
+int CountRoots2(TreeNode* node);
+int countLeftPath(BST* bst);
--- TreePrintLibrary.h
+++
@@ -1,7 +0,0 @@
-#ifndef _TREE_PRINT_LIBRARY_HEADER_
-
-struct TreeNode;// forward declaration
-
-extern __declspec(dllimport) void print_ascii_tree(TreeNode* t);
-
-#endif // !_TREE_PRINT_LIBRARY_HEADER_
